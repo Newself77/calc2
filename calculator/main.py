406191e097e9698678cc805ce3dd38ebbd7a3161
Newself77 <gw22@njit.edu>
2021-11-13 20:26:09 -0500

    Checking to make sure it is complete

--- calculator/main.py
+++ calculator/main.py
@@ -1,25 +1,43 @@
 """ This is the increment function"""
+# first import the addition namespace
+from calc.addition import Addition
+
+
 class Calculator:
     """ This is the Calculator class"""
+    history = []
 
-    result = 3
-    def get_result(self):
-        """ Get Result of Calculation"""
-        return self.result
+    def __init__(self):
+        self.result = None
 
-    def add_number(self, value_a):
+    @staticmethod
+    def add_calculation_to_history(calculation):
+        Calculator.history.append(calculation)
+        return True
+
+    @staticmethod
+    def get_result_of_last_calculation_added_to_history():
+        # -1 get the last item to the list automatically and you can expect it to have the get results method
+        return Calculator.history[-1].getResults()
+
+    @staticmethod
+    def add_number(value_a, value_b):
         """ adds number to result"""
-        self.result = self.result + value_a
-        return self.result
-    def subtract_number(self, value_a):
+        # create an addition object using a factory we created on the calculation class
+        addition = Addition.create(value_a, value_b)
+        # addition = Addition(value_a,value_b) <-this is not good but will work. It will be repeated to much
+        Calculator.add_calculation_to_history(addition)
+        return Calculator.get_result_of_last_calculation_added_to_history()
+
+    @staticmethod
+    def subtract_number(value_a, value_b):
         """ subtract number from result"""
-        self.result = self.result - value_a
-        return self.result
-    def multiply_numbers(self, vale_a, value_b):
-        """ tests multiplication of two numbers"""
-        self.result = vale_a * value_b
-        return self.result
-    def divide_numbers(self, value_a, value_b):
-        """ tests divsion of two numbers """
-        self.result = value_a / value_b
-        return self.result
+        return value_a - value_b
+
+    @staticmethod
+    def multiply_numbers(vale_a, value_b):
+        """ multiplication of two numbers and store the result"""
+        return vale_a * value_b
+
+    def get_result(self):
+        pass
