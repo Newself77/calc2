ea7171268a5916e517a338d5c80b10fd184af66b
Newself77 <gw22@njit.edu>
2021-11-19 03:23:00 -0500

    Trying to pass for Travis

--- calc/calculator.py
+++ calc/calculator.py
@@ -2,37 +2,33 @@
 from calc.calculations.addition import Addition
 from calc.calculations.subtraction import Subtraction
 from calc.calculations.multiplication import Multiplication
+from calc.history.calculations import Calculations
+
 
 class Calculator:
     """ This is the Calculator class"""
-    history = []
+
     @staticmethod
     def add_numbers(*args):
         """ adds list of numbers"""
-        addition = Addition(args)
-        Calculator.history.append(addition)
-        return addition.get_result()
-    @staticmethod
-    def clear_history():
-        """ Clear the calculation history"""
-        Calculator.history.clear()
-        return True
-    @staticmethod
-    def get_calculation(num):
-        """ get a specific calculation from history"""
-        return Calculator.history[num]
-    @staticmethod
-    def get_calculation_last():
-        """ get last calculation from history"""
-        return Calculator.history[-1]
+        calculation = Addition(args)
+        Calculations.add_calculations(calculation)
+        return calculation.get_result()
+
     @staticmethod
     def subtract_numbers(*args):
         """ subtract a list of numbers from result"""
-        subtraction = Subtraction(args)
-        Calculator.history.append(subtraction)
-        return subtraction.get_result()
+        calculation = Subtraction(args)
+        Calculations.add_calculations(calculation)
+        return calculation.get_result()
+
     @staticmethod
     def multiply_numbers(*args):
         """ multiplication number from result"""
-        multiplication = Multiplication(args)
-        return multiplication.get_result()
+        calculation = Multiplication(args)
+        Calculations.add_calculations(calculation)
+        return calculation.get_result()
+
+    @classmethod
+    def clear_history(cls):
+        pass
--- calc/history/calculations.py
+++ calc/history/calculations.py
@@ -1,11 +1,27 @@
 """Calculation history Class"""
 class Calculations:
     history = []
-    # pylint: disable=too-few-public-methods
+     # pylint: disable=too-few-public-methods
     @staticmethod
     def clear_history():
         Calculations.history.clear()
         return True
     @staticmethod
+    def count_history():
+        return Len(Calculations.history)
+    @staticmethod
     def get_last_calculation():
         return Calculations.history[-1]
+
+    @staticmethod
+    def get_first_calculation():
+        return Calculations.history[-1]
+
+    def get_calculation(self):
+        """ get a specific calculation from history"""
+        return Calculations.history[self]
+
+    @staticmethod
+    def add_calculations(calculation):
+        """ get a specific calculation from history"""
+        return Calculations.history.append(calculation)
