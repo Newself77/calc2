a9c32ceacfec76ec53a62394ad1cb58e3d8c5a88
Newself77 <gw22@njit.edu>
2021-11-19 11:40:22 -0500

    Final commit. 100% coverage 12 passed 1 failed

--- calc/history/calculations.py
+++ calc/history/calculations.py
@@ -2,30 +2,35 @@
 
 
 class Calculations:
+    """ get a specific calculation from history"""
     history = []
 
     # pylint: disable=too-few-public-methods
     @staticmethod
     def clear_history():
-        """Clearing the history"""
+        """Clear history"""
         Calculations.history.clear()
         return True
 
     @staticmethod
     def count_history():
+        """ get a specific calculation from history"""
         return len(Calculations.history)
 
     @staticmethod
     def get_last_calculation():
+        """ get a specific calculation from history"""
         return Calculations.history[-1]
 
     @staticmethod
     def get_first_calculation():
+        """ get a specific calculation from history"""
         return Calculations.history[-1]
 
-    def get_calculation(self):
+    @staticmethod
+    def get_calculation(num):
         """ get a specific calculation from history"""
-        return Calculations.history[self]
+        return Calculations.history[num]
 
     @staticmethod
     def add_calculations(calculation):
